1c0d07e2720ea9c7e4ff71ac4517d3b1351cdec3
KokInside <ivankhvostov2005@gmail.com>
2025-11-01 23:42:40 +0300

    add paginator

--- askme/core/views.py
+++ askme/core/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.core.paginator import Paginator
+
+
+questions = [
+	{
+		'id': i,
+		'title': "question № " + str(i) + " title",
+		'text': "question № " + str(i) + " text",
+		'tags': ["c++", "python", "Go"]
+	} for i in range(96)
+]
+
+def paginate(questions_list, request: HttpRequest, per_page = 10):
+	pag = Paginator(questions_list, per_page)
+
+	page_number = int(request.GET.get('page', 1))
+
+	if (page_number < 0):
+		page_number = 1
+
+	return pag.get_page(page_number), pag.get_elided_page_range(page_number)
 
 
 def home(request):
-	return render(request, "index.html")
+	page, page_numbers = paginate(questions, request, 3)
+
+	context = {'page': page, 'page_numbers': page_numbers}
+
+	return render(request, "index.html", context)
 
 def hot(request):
+
 	return render(request, "hot.html")
 
 def tag(request):
+
 	pass
 
 def question(request):
