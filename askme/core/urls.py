5d74ce097c55aa5414eeab5bf4f874af293bf366
KokInside <ivankhvostov2005@gmail.com>
2025-11-01 23:44:05 +0300

    add tag and question urls with parameters

--- askme/core/urls.py
+++ askme/core/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
+from django.urls import path, re_path, include
 
 from core.views import *
+
+tagurlpatterns = [
+	path("", tag, name = "tags_page"),
+	path("<str:tag_name>/", tag, name = "tag_name"),
+]
+
+questionurlpatterns = [
+	path("", question, name = "question_page"),
+	path("<int:question_id>/", question, name = "question_id"),
+]
 
 coreurlpatterns = [
 	path("", home, name = "home"),
 	path("hot/", hot, name = "hot"),
-	path("tag/", tag, name = "tag"),
-	path("question/", question, name = "question"),
 	path("login/", login, name = "login"),
 	path("signup/", signup, name = "signup"),
 	path("ask/", ask, name = "ask"),
+
+	re_path(r"^tag/", include(tagurlpatterns), name = "tag"),
+	re_path(r"question/", include(questionurlpatterns), name = "question"),
 ]
