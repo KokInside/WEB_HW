2b86779e1c3ef0b035cc179d9836e125b35e8262
KokInside <ivankhvostov2005@gmail.com>
2025-11-12 17:14:57 +0300

    create question manager

---
+++ askme/core/managers/QuestionManager.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class QuestionManager(models.Manager):
+
+	def get_queryset(self):
+		return super().get_queryset()
+	
+	def get_new(self):
+		return super().get_queryset().order_by("-created_at", "title")
+	
+	def get_hot(self):
+		return super().get_queryset().order_by("-likes")
+	
+	def get_by_id(self, question_id):
+		return super().get_queryset().get(id = question_id)
