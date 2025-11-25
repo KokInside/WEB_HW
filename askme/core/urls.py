537605d02f8f5314c5cbe5bf8a19eccab9541396
KokInside <ivankhvostov2005@gmail.com>
2025-11-26 02:01:46 +0300

    change views to class based views

--- askme/core/urls.py
+++ askme/core/urls.py
@@ -4,17 +4,21 @@ from core.views import *
 
 tagurlpatterns = [
 	#path("", tag, name = "tags_page"),
-	path("<str:tag_name>/", tag, name = "tag"),
+	#path("<str:tag_name>/", tag, name = "tag"),
+	path("<str:tag_name>/", TagView.as_view(), name = "tag"),
 ]
 
 questionurlpatterns = [
 	#path("", question, name = "question_page"),
-	path("<int:question_id>/", question, name = "question"),
+	#path("<int:question_id>/", question, name = "question"),
+	path("<int:question_id>/", QuestionView.as_view(), name = "question"),
 ]
 
 coreurlpatterns = [
-	path("", home, name = "home"),
-	path("hot/", hot, name = "hot"),
+	#path("", home, name = "home"),
+	path("", IndexView.as_view(), name = "home"),
+	#path("hot/", hot, name = "hot"),
+	path("hot/", HotView.as_view(), name = "hot"),
 	path("login/", login, name = "login"),
 	path("signup/", signup, name = "signup"),
 	path("ask/", ask, name = "ask"),
--- askme/core/views.py
+++ askme/core/views.py
@@ -5,6 +5,14 @@ from django.core.paginator import Paginator
 
 from core.models import Question, Tag
 
+from django.views.generic import TemplateView
+
+from django.utils.decorators import method_decorator
+
+from django.views.decorators.cache import never_cache
+
+from django.shortcuts import get_object_or_404
+
 
 def paginate(questions, request: HttpRequest):
 
@@ -23,67 +31,162 @@ def paginate(questions, request: HttpRequest):
 	return page, page_numbers
 
 
-def home(request):
+@method_decorator(never_cache, name = 'dispatch')
+class IndexView(TemplateView):
+	http_method_names = ["get",]
+	template_name = "index.html"
 
-	questions = Question.qManager.get_new()
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
 
-	page, page_numbers = paginate(questions, request)
+		questions = Question.qManager.get_new()
 
-	context = {'page': page, 'page_numbers': page_numbers}
+		page, page_numbers = paginate(questions, self.request)
 
-	return render(request, "index.html", context)
-
-
-def hot(request):
-
-	questions = Question.qManager.get_hot()
-
-	page, page_numbers = paginate(questions, request)
-
-	context = {'page': page, 'page_numbers': page_numbers}
-
-	return render(request, "hot.html", context)
+		context['page'] = page
+		context['page_numbers'] = page_numbers
+		
+		return context
 
 
-def tag(request, tag_name):
 
-	try:
-		questions = Tag.objects.get(name=tag_name).questions.order_by("-created_at")
-
-		page, page_numbers = paginate(questions, request)
-
-		context = {'page': page, 'page_numbers': page_numbers, 'tag': tag_name}
-
-		return render(request, "tag.html", context)
-	
-	except Tag.DoesNotExist:
-		return HttpResponseNotFound("<h1>Tag not found</h1>")
-	
-	except Question.MultipleObjectsReturned:
-		return
+#def home(request):
+#
+#	questions = Question.qManager.get_new()
+#
+#	page, page_numbers = paginate(questions, request)
+#
+#	context = {'page': page, 'page_numbers': page_numbers}
+#
+#	return render(request, "index.html", context)
 
 
-def question(request, question_id):
+@method_decorator(never_cache, name = 'dispatch')
+class HotView(TemplateView):
+	http_method_names = ['get',]
+	template_name = 'hot.html'
 
-	try:
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
 
-		question = Question.qManager.get_by_id(question_id)
+		questions = Question.qManager.get_hot()
+
+		page, page_numbers = paginate(questions, self.request)
+
+		context['page'] = page
+		context['page_numbers'] = page_numbers
+
+		return context
+
+
+#def hot(request):
+#
+#	questions = Question.qManager.get_hot()
+#
+#	page, page_numbers = paginate(questions, request)
+#
+#	context = {'page': page, 'page_numbers': page_numbers}
+#
+#	return render(request, "hot.html", context)
+
+
+@method_decorator(never_cache, name = 'dispatch')
+class TagView(TemplateView):
+	http_method_names = ['get',]
+	template_name = 'tag.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		tag_name = self.kwargs.get('tag_name')
+
+		questions = get_object_or_404(Tag, name = tag_name).questions.order_by("-created_at")
+
+		page, page_numbers = paginate(questions, self.request)
+
+		context['page'] = page
+		context['page_numbers'] = page_numbers
+
+		return context
+
+
+#def tag(request, tag_name):
+#
+#	try:
+#		questions = Tag.objects.get(name=tag_name).questions.order_by("-created_at")
+#
+#		page, page_numbers = paginate(questions, request)
+#
+#		context = {'page': page, 'page_numbers': page_numbers, 'tag': tag_name}
+#
+#		return render(request, "tag.html", context)
+#	
+#	except Tag.DoesNotExist:
+#		return HttpResponseNotFound("<h1>Tag not found</h1>")
+#	
+#	except Question.MultipleObjectsReturned:
+#		return
+
+
+@method_decorator(never_cache, name = 'dispatch')
+class QuestionView(TemplateView):
+	http_method_names = ['get',]
+	template_name = 'question.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		question_id = self.kwargs.get('question_id')
+
+		#question = Question.qManager.get_by_id(question_id)
+
+		question = get_object_or_404(Question.qManager, id = question_id)
 
 		answers = question.answer_set.order_by("-likes")
 
-		context = {'question': question, 'answers': answers}
+		context['question'] = question
+		context['answers'] = answers
 
-		return render(request, "question.html", context)
+		return context
 
-	except Question.DoesNotExist:
-		return HttpResponseNotFound("<h1>Question not found</h1>")
-	
-	except Question.MultipleObjectsReturned:
-		return
+
+#def question(request, question_id):
+#
+#	# добавить get_or_404
+#
+#	try:
+#
+#		question = Question.qManager.get_by_id(question_id)
+#
+#		answers = question.answer_set.order_by("-likes")
+#
+#		context = {'question': question, 'answers': answers}
+#
+#		return render(request, "question.html", context)
+#
+#	except Question.DoesNotExist:
+#		return HttpResponseNotFound("<h1>Question not found</h1>")
+#	
+#	except Question.MultipleObjectsReturned:
+#		return
+
+
+@method_decorator(never_cache, name = 'dispatch')
+class SettingsView(TemplateView):
+	http_method_names = ['get',]
+	template_name = 'settings.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		#context[]
+		
+		return context 
 
 
 def settings(request):
 	context = {'registered': True}
+	print(request.user.is_authenticated)
 	return render(request, "settings.html", context)
 
 
