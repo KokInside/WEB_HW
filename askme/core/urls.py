ba0edf3185c8ad870cb06cda5324718056e7c1a8
KokInside <ivankhvostov2005@gmail.com>
2025-12-20 03:34:56 +0300

    add auth urls

--- askme/core/urls.py
+++ askme/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, re_path, include
 
+from django.contrib.auth.views import LoginView
+
+from django.conf.urls.static import static
+
 from core.views import *
+
+from askme.settings import MEDIA_URL, MEDIA_ROOT
+
 
 tagurlpatterns = [
 	#path("", tag, name = "tags_page"),
@@ -14,16 +21,27 @@ questionurlpatterns = [
 	path("<int:question_id>/", QuestionView.as_view(), name = "question"),
 ]
 
+profileurlpatterns = [
+	path("", settings, name = "profile"),
+	path("edit/", settings, name = "edit_profile")
+]
+
 coreurlpatterns = [
-	#path("", home, name = "home"),
 	path("", IndexView.as_view(), name = "home"),
-	#path("hot/", hot, name = "hot"),
 	path("hot/", HotView.as_view(), name = "hot"),
-	path("login/", login, name = "login"),
-	path("signup/", signup, name = "signup"),
-	path("ask/", ask, name = "ask"),
-	path("settings/", settings, name = "settings"),
+
+	path("login-username/", UsernameLoginView.as_view(), name = "login_username"),
+	path("login-email/", EmailLoginView.as_view(), name = "login_email"),
+
+	path("signup/", SignupView.as_view(), name = "signup"),
+	path("ask/", AskView.as_view(), name = "ask"),
+	#path("settings/", settings, name = "settings"),
 
 	path("tag/", include(tagurlpatterns)),
 	path("question/", include(questionurlpatterns)),
+	path("logout/", LogoutView, name="logout"),
+
+	path("profile/", include(profileurlpatterns)),
 ]
+
+coreurlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
