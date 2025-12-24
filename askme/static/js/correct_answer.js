aa96abfc57d97fd4b482510b0cc2f44c94a97fbc
KokInside <ivankhvostov2005@gmail.com>
2025-12-25 02:55:41 +0300

    create correct answer AJAX js

---
+++ askme/static/js/correct_answer.js
@@ -0,0 +1,32 @@
+function correct(question_id, answer_id, csrf) {
+	fetch(`/api/${question_id}/${answer_id}/correct/`, {
+		method: "POST",
+		headers: {
+			'X-CSRFToken': csrf
+		}
+	}).then((res) => {
+		if (res.status >= 400) {
+			console.log("something wrong: " + res.json.info);
+		}
+		return res;
+	}).then((res) => {
+		return res.json();
+	}).then((res) => {
+		const currentCorrect = res.current_correct;
+
+		const answers_to_uncorrect = res.answers_to_uncorrect
+
+		answers_to_uncorrect.forEach((answerID) => {
+			const element = document.getElementById(`${answerID}--correct`);
+			element.hidden = true;
+		});
+
+		if (currentCorrect) {
+			const answerCorrect = document.getElementById(`${answer_id}--correct`);
+			answerCorrect.hidden = true;			
+		} else {
+			const answerCorrect = document.getElementById(`${answer_id}--correct`);
+			answerCorrect.hidden = false;
+		}
+	})
+}
