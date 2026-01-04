d5047f8f51f7a420583e8d5f80330bfe58ab15fe
KokInside <ivankhvostov2005@gmail.com>
2026-01-05 02:43:58 +0300

    add allowed & trusted hosts

--- askme/askme/settings.py
+++ askme/askme/settings.py
@@ -143,11 +143,10 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ask.kokinside"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8080", "http://localhost:8000"
-
+    "http://localhost:8080", "http://localhost:8000", "http://ask.kokinside:8822"
 ]
 
 # Default primary key field type
