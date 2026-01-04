0545e2625d5a98e7d574e1fc8eb976159743928a
KokInside <ivankhvostov2005@gmail.com>
2026-01-05 02:42:42 +0300

    create gunicorn config

---
+++ askme/conf/gunicorn.conf.py
@@ -0,0 +1,23 @@
+import multiprocessing
+
+# WSGI application
+wsgi_app = "askme.wsgi:application"
+
+# Server adres
+bind = "127.0.0.1:8033"
+
+# Workers configurations
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# Logging
+#accesslog = "logs/gunicorn/gunicorn.access.log"
+#errorlog = "logs/gunicorn/gunicorn.error.log"
+
+# def on_starting(server):
+# 	pass
+
+# def on_reload(server):
+# 	pass
+
+# def when_ready(server):
+# 	pass
